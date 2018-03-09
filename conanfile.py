65dcd12873846f59178797652af934a576d5feff
Eric Pederson <eric.pederson@tradeweb.com>
2018-03-09 10:28:18 -0500

    Simplify build.py. Change conanfile.py checks for settings.os == "Windows" to settings.compiler == "Visual Studio"

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,7 @@ class CAFConan(ConanFile):
     name = "caf"
     version = "0.15.5"
     description = "An open source implementation of the Actor Model in C++"
-    url = "http://actor-framework.org"
+    url = "https://github.com/actor-framework/actor-framework"
     license = "BSD-3-Clause, BSL-1.0"
     exports = ["LICENSE.md"]
     exports_sources = ["CMakeLists.txt"]
@@ -18,9 +18,9 @@ class CAFConan(ConanFile):
     source_subfolder = "source_subfolder"
     settings = "os", "compiler", "build_type", "arch"
     options = {
-        "shared": [True, False], 
+        "shared": [True, False],
         "log_level": ["NONE", "ERROR", "WARNING", "INFO", "DEBUG", "TRACE"]
-   }
+    }
     default_options = "shared=False", "log_level=NONE"
 
     def source(self):
@@ -30,7 +30,7 @@ class CAFConan(ConanFile):
         download_url = project_url + archive_path + self.version + archive_ext
         tools.get(download_url)
         os.rename("actor-framework-" + self.version, self.source_subfolder)
-        
+
     def configure(self):
         if self.settings.compiler == "gcc":
             if str(self.settings.compiler.version) < "4.8":
@@ -44,7 +44,7 @@ class CAFConan(ConanFile):
         cmake = CMake(self)
         cmake.parallel = True
         cmake.definitions["CMAKE_CXX_STANDARD"] = "11"
-        if tools.os_info.is_windows or self.settings.arch == "x86":
+        if self.settings.compiler == "Visual Studio" or self.settings.arch == "x86":
             cmake.definitions["CAF_NO_OPENSSL"] = "ON"
         for define in ["CAF_NO_EXAMPLES", "CAF_NO_TOOLS", "CAF_NO_UNIT_TESTS", "CAF_NO_PYTHON"]:
             cmake.definitions[define] = "ON"
@@ -63,26 +63,26 @@ class CAFConan(ConanFile):
         cmake.build()
 
     def package(self):
-        dst_include_dir = os.path.join("include","caf")
+        dst_include_dir = os.path.join("include", "caf")
         self.copy("LICENSE*", src=self.source_subfolder)
-        self.copy("*.hpp",    dst=dst_include_dir,  src=os.path.join(self.source_subfolder, "libcaf_core", "caf"))
-        self.copy("*.hpp",    dst=dst_include_dir,  src=os.path.join(self.source_subfolder, "libcaf_io", "caf"))
-        self.copy("*.dylib",  dst="lib", keep_path=False)
-        self.copy("*.so",     dst="lib", keep_path=False)
-        self.copy("*.so.*",  dst="lib", keep_path=False)
-        self.copy("*.a",      dst="lib", keep_path=False)
-        self.copy("*.lib",    dst="lib", keep_path=False)
-        self.copy("*.dll",    dst="bin", keep_path=False)
+        self.copy("*.hpp", dst=dst_include_dir, src=os.path.join(self.source_subfolder, "libcaf_core", "caf"))
+        self.copy("*.hpp", dst=dst_include_dir, src=os.path.join(self.source_subfolder, "libcaf_io", "caf"))
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.so.*", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
 
     def package_info(self):
         tools.collect_libs(self)
-        
+
         if self.options.shared:
             self.cpp_info.libs.extend(["caf_io", "caf_core"])
         if not self.options.shared:
             self.cpp_info.libs.extend(["caf_io_static", "caf_core_static"])
 
-        if self.settings.os == "Windows":
+        if self.settings.compiler == "Visual Studio":
             if not self.options.shared:
                 self.cpp_info.libs.append('ws2_32')
                 self.cpp_info.libs.append('iphlpapi')
