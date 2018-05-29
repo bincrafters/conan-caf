ca44330b66e324761bfcbbef726d472dfe9f0ca8
Uilian Ries <uilianries@gmail.com>
2018-05-29 09:21:08 -0300

    Improve test package
    - Add logo
    - Add author
    - Replace test package by CAF example
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,7 +1,41 @@
+#include <string>
+#include <iostream>
+#include <chrono>
+
 #include "caf/all.hpp"
 
-int main()
-{
-    caf::actor_system_config cfg;
-    return 0;
+caf::behavior mirror(caf::event_based_actor* self) {
+  // return the (initial) actor behavior
+  return {
+    // a handler for messages containing a single std::string
+    // that replies with a std::string
+    [=](const std::string& what) -> std::string {
+      // prints "Hello World!" via aout (thread-safe cout wrapper)
+      aout(self) << what << std::endl;
+      // reply "!dlroW olleH"
+      return std::string(what.rbegin(), what.rend());
+    }
+  };
+}
+
+void hello_world(caf::event_based_actor* self, const caf::actor& buddy) {
+  // send "Hello World!" to our buddy ...
+  self->request(buddy, std::chrono::seconds(10), "Hello World!").then(
+    // ... wait up to 10s for a response ...
+    [=](const std::string& what) {
+      // ... and print it
+      aout(self) << what << std::endl;
+    }
+  );
+}
+
+int main() {
+  // our CAF environment
+  caf::actor_system_config cfg;
+  caf::actor_system system{cfg};
+  // create a new actor that calls 'mirror()'
+  auto mirror_actor = system.spawn(mirror);
+  // create another actor that calls 'hello_world(mirror_actor)';
+  system.spawn(hello_world, mirror_actor);
+  // system will wait until both actors are destroyed before leaving main
 }
